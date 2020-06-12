5eea2cfc4ee2a9a6f46f6fd990e4799eb85a190d
Olivier Goulpeau <olivier.goulpeau@ledger.fr>
2020-06-12 18:15:59 +0200

    multi-account: pass `privateKeyData` as argument and removed `static` variable from `algo_keys.c` (WIP).
    WIP: not optimal yet as `privateDataKey` becomes a 64-bytes array
    allocated on the stack.

--- src/algo_keys.h
+++ src/algo_keys.h
@@ -2,6 +2,7 @@
 
 extern uint8_t publicKey[32];
 
-void algorand_key_derive(uint32_t accountId);
-void algorand_private_key(cx_ecfp_private_key_t *privateKey);
-void algorand_public_key(uint8_t *buf);
+void algorand_key_derive(uint32_t accountId, uint8_t *privateKeyData);
+void algorand_private_key(uint8_t *privateKeyData,
+                          cx_ecfp_private_key_t *privateKey);
+void algorand_public_key(uint8_t *privateKeyData, uint8_t *buf);
