669eeb33db80801084bc979aeb46dbcd4d18ed90
Olivier Goulpeau <olivier.goulpeau@ledger.fr>
2020-06-13 07:12:34 +0200

    multi-account: made `algorand_public_key()` return public key size.

--- src/algo_keys.h
+++ src/algo_keys.h
@@ -2,4 +2,4 @@
 
 
 void algorand_key_derive(uint32_t accountId, cx_ecfp_private_key_t *privateKey);
-void algorand_public_key(cx_ecfp_private_key_t *privateKey, uint8_t *buf);
+int algorand_public_key(cx_ecfp_private_key_t *privateKey, uint8_t *buf);
