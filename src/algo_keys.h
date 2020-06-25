8c37b8505ac708f3da54f6fe54ef9f2ffb3306ff
Olivier Goulpeau <olivier.goulpeau@ledger.fr>
2020-06-25 09:42:30 +0200

    multi-account: pass `privateKey` input argument as `const` pointer to `algorand_public_key()`.

--- src/algo_keys.h
+++ src/algo_keys.h
@@ -1,5 +1,4 @@
 #include "cx.h"
 
-
 void algorand_key_derive(uint32_t accountId, cx_ecfp_private_key_t *privateKey);
-int algorand_public_key(cx_ecfp_private_key_t *privateKey, uint8_t *buf);
+int algorand_public_key(const cx_ecfp_private_key_t *privateKey, uint8_t *buf);
