3dc24148628a7393db48f842c3620283212bc7f7
Nickolai Zeldovich <nickolai@csail.mit.edu>
2021-02-01 19:19:44 -0500

    regen glyphs.c, glyphs.h

--- src/glyphs.c
+++ src/glyphs.c
@@ -1,108 +1,116 @@
 #include "glyphs.h"
-unsigned int const C_icon_crossmark_colors[] = {
-  0x00000000,
-  0x00ffffff,
+unsigned int const C_icon_crossmark_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
 };
-
+	
 unsigned char const C_icon_crossmark_bitmap[] = {
-  0x00, 0x80, 0x01, 0xe6, 0xc0, 0x71, 0x38, 0x38, 0x07, 0xfc, 0x00, 0x1e, 0x80, 0x07, 0xf0, 0x03,
-  0xce, 0xc1, 0xe1, 0x38, 0x70, 0x06, 0x18, 0x00, 0x00,
-};
-#ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_crossmark = { GLYPH_icon_crossmark_WIDTH, GLYPH_icon_crossmark_HEIGHT, 1, C_icon_crossmark_colors, C_icon_crossmark_bitmap };
-        #endif // OS_IO_SEPROXYHAL
-#include "glyphs.h"
-unsigned int const C_icon_dashboard_colors[] = {
-  0x00000000,
-  0x00ffffff,
-};
+0x00, 0x80, 0x01, 0xe6, 0xc0, 0x71, 0x38, 0x38, 0x07, 0xfc, 0x00, 0x1e, 0x80, 0x07, 0xf0, 0x03, 
+  0xce, 0xc1, 0xe1, 0x38, 0x70, 0x06, 0x18, 0x00, 0x00, };
 
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_crossmark = { GLYPH_icon_crossmark_WIDTH, GLYPH_icon_crossmark_HEIGHT, 1, C_icon_crossmark_colors, C_icon_crossmark_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_dashboard_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
 unsigned char const C_icon_dashboard_bitmap[] = {
-  0xe0, 0x01, 0xfe, 0xc1, 0xff, 0x38, 0x70, 0x06, 0xd8, 0x79, 0x7e, 0x9e, 0x9f, 0xe7, 0xe7, 0xb9,
-  0x01, 0xe6, 0xc0, 0xf1, 0x3f, 0xf8, 0x07, 0x78, 0x00,
-};
-#ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_dashboard = { GLYPH_icon_dashboard_WIDTH, GLYPH_icon_dashboard_HEIGHT, 1, C_icon_dashboard_colors, C_icon_dashboard_bitmap };
-        #endif // OS_IO_SEPROXYHAL
-#include "glyphs.h"
-unsigned int const C_icon_dashboard_x_colors[] = {
-  0x00000000,
-  0x00ffffff,
-};
+0xe0, 0x01, 0xfe, 0xc1, 0xff, 0x38, 0x70, 0x06, 0xd8, 0x79, 0x7e, 0x9e, 0x9f, 0xe7, 0xe7, 0xb9, 
+  0x01, 0xe6, 0xc0, 0xf1, 0x3f, 0xf8, 0x07, 0x78, 0x00, };
 
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_dashboard = { GLYPH_icon_dashboard_WIDTH, GLYPH_icon_dashboard_HEIGHT, 1, C_icon_dashboard_colors, C_icon_dashboard_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_dashboard_x_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
 unsigned char const C_icon_dashboard_x_bitmap[] = {
-  0x00, 0x00, 0x00, 0x00, 0x0c, 0x80, 0x07, 0xf0, 0x03, 0xfe, 0xc1, 0xff, 0xf0, 0x3f, 0xf0, 0x03,
-  0xcc, 0x00, 0x33, 0xc0, 0x0c, 0x00, 0x00, 0x00, 0x00,
-};
-#ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_dashboard_x = { GLYPH_icon_dashboard_x_WIDTH, GLYPH_icon_dashboard_x_HEIGHT, 1, C_icon_dashboard_x_colors, C_icon_dashboard_x_bitmap };
-        #endif // OS_IO_SEPROXYHAL
-#include "glyphs.h"
-unsigned int const C_icon_down_colors[] = {
-  0x00000000,
-  0x00ffffff,
-};
+0x00, 0x00, 0x00, 0x00, 0x0c, 0x80, 0x07, 0xf0, 0x03, 0xfe, 0xc1, 0xff, 0xf0, 0x3f, 0xf0, 0x03, 
+  0xcc, 0x00, 0x33, 0xc0, 0x0c, 0x00, 0x00, 0x00, 0x00, };
 
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_dashboard_x = { GLYPH_icon_dashboard_x_WIDTH, GLYPH_icon_dashboard_x_HEIGHT, 1, C_icon_dashboard_x_colors, C_icon_dashboard_x_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_down_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
 unsigned char const C_icon_down_bitmap[] = {
-  0x41, 0x11, 0x05, 0x01,
-};
-#ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_down = { GLYPH_icon_down_WIDTH, GLYPH_icon_down_HEIGHT, 1, C_icon_down_colors, C_icon_down_bitmap };
-        #endif // OS_IO_SEPROXYHAL
-#include "glyphs.h"
-unsigned int const C_icon_left_colors[] = {
-  0x00000000,
-  0x00ffffff,
-};
+0x41, 0x11, 0x05, 0x01, };
 
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_down = { GLYPH_icon_down_WIDTH, GLYPH_icon_down_HEIGHT, 1, C_icon_down_colors, C_icon_down_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_left_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
 unsigned char const C_icon_left_bitmap[] = {
-  0x48, 0x12, 0x42, 0x08,
-};
-#ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_left = { GLYPH_icon_left_WIDTH, GLYPH_icon_left_HEIGHT, 1, C_icon_left_colors, C_icon_left_bitmap };
-        #endif // OS_IO_SEPROXYHAL
-#include "glyphs.h"
-unsigned int const C_icon_right_colors[] = {
-  0x00000000,
-  0x00ffffff,
-};
+0x48, 0x12, 0x42, 0x08, };
 
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_left = { GLYPH_icon_left_WIDTH, GLYPH_icon_left_HEIGHT, 1, C_icon_left_colors, C_icon_left_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_right_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
 unsigned char const C_icon_right_bitmap[] = {
-  0x21, 0x84, 0x24, 0x01,
-};
-#ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_right = { GLYPH_icon_right_WIDTH, GLYPH_icon_right_HEIGHT, 1, C_icon_right_colors, C_icon_right_bitmap };
-        #endif // OS_IO_SEPROXYHAL
-#include "glyphs.h"
-unsigned int const C_icon_up_colors[] = {
-  0x00000000,
-  0x00ffffff,
-};
+0x21, 0x84, 0x24, 0x01, };
 
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_right = { GLYPH_icon_right_WIDTH, GLYPH_icon_right_HEIGHT, 1, C_icon_right_colors, C_icon_right_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_up_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
 unsigned char const C_icon_up_bitmap[] = {
-  0x08, 0x8a, 0x28, 0x08,
-};
-#ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_up = { GLYPH_icon_up_WIDTH, GLYPH_icon_up_HEIGHT, 1, C_icon_up_colors, C_icon_up_bitmap };
-        #endif // OS_IO_SEPROXYHAL
-#include "glyphs.h"
-unsigned int const C_icon_validate_14_colors[] = {
-  0x00000000,
-  0x00ffffff,
-};
+0x08, 0x8a, 0x28, 0x08, };
 
-unsigned char const C_icon_validate_14_bitmap[] = {
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0x00, 0x38, 0x00, 0x67, 0xe0, 0x38, 0x1c, 0x9c, 0x03,
-  0x7e, 0x00, 0x0f, 0x80, 0x01, 0x00, 0x00, 0x00, 0x00,
-};
 #ifdef OS_IO_SEPROXYHAL
-        #include "os_io_seproxyhal.h"
-        const bagl_icon_details_t C_icon_validate_14 = { GLYPH_icon_validate_14_WIDTH, GLYPH_icon_validate_14_HEIGHT, 1, C_icon_validate_14_colors, C_icon_validate_14_bitmap };
-        #endif // OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_up = { GLYPH_icon_up_WIDTH, GLYPH_icon_up_HEIGHT, 1, C_icon_up_colors, C_icon_up_bitmap };
+#endif // OS_IO_SEPROXYHAL
+#include "glyphs.h"
+unsigned int const C_icon_validate_14_colors[]
+ = {
+  0x00000000, 
+  0x00ffffff, 
+};
+	
+unsigned char const C_icon_validate_14_bitmap[] = {
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0x00, 0x38, 0x00, 0x67, 0xe0, 0x38, 0x1c, 0x9c, 0x03, 
+  0x7e, 0x00, 0x0f, 0x80, 0x01, 0x00, 0x00, 0x00, 0x00, };
+
+#ifdef OS_IO_SEPROXYHAL
+#include "os_io_seproxyhal.h"
+const bagl_icon_details_t C_icon_validate_14 = { GLYPH_icon_validate_14_WIDTH, GLYPH_icon_validate_14_HEIGHT, 1, C_icon_validate_14_colors, C_icon_validate_14_bitmap };
+#endif // OS_IO_SEPROXYHAL
