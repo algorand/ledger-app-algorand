5813ca7c397ebb41c1881a1cdf531acecfebe0b6
algoidan <f>
2021-04-18 14:27:29 +0300

    adding tests for for foreign apps, assets and accounts. + limits tests

--- tests/test/test_sign_msgpack_teal.py
+++ tests/test/test_sign_msgpack_teal.py
@@ -49,6 +49,7 @@ def app_call_txn():
                                     gen="testnet-v1.0",
                                     gh="SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",flat_fee=True)
     txn = algosdk.future.transaction.ApplicationCallTxn(sender="YK54TGVZ37C7P76GKLXTY2LAH2522VD3U2434HRKE7NMXA65VHJVLFVOE4", sp=local_sp, index=68,
+                                                        foreign_apps=[55], foreign_assets=[31566704], accounts=["7PKXMJB2577SQ6R6IGYRAZQ27TOOOTIGTOQGJB3L5SGZFBVVI4AHMKLCEI","NWBZBIROXZQEETCDKX6IZVVBV4EY637KCIX56LE5EHIQERCTSDYGXWG6PU"],
                                                         on_complete=transaction.OnComplete.NoOpOC.real )
     return txn
 
@@ -74,6 +75,10 @@ def get_expected_messages_for_call_pgm(current_txn):
                  ['genesis hash', current_txn.genesis_hash.lower()],
                  ['app id', str(current_txn.index)],
                  ['on completion', operation_text.lower()],
+                 ['foreign app 0', str(current_txn.foreign_apps[0])],
+                 ['foreign asset 0', str(current_txn.foreign_assets[0])],
+                 ['app account 0', str(current_txn.accounts[0]).lower()],
+                 ['app account 1', str(current_txn.accounts[1]).lower()],
                  ['sign', 'transaction']]
                  
     return messages
@@ -94,7 +99,9 @@ def get_expected_messages_for_create_pgm(current_txn):
 
 
 txn_labels = {
-    'review', 'txn type','sender','fee (alg)', 'genesis id', 'genesis hash', 'app id', 'on completion','global schema', 'local schema',  'apprv (sha256)', 'clear (sha256)','sign'
+    'review', 'txn type','sender','fee (alg)', 'genesis id', 'genesis hash', 'app id', 'on completion', 
+    'foreign app 0', 'foreign asset 0','app account 0', 'app account 1',
+    'global schema', 'local schema',  'apprv (sha256)', 'clear (sha256)','sign'
 } 
 
 conf_label = "sign"
@@ -113,6 +120,58 @@ def test_sign_msgpack_call_app_validate_display(dongle, app_call_txn):
     logging.info(get_expected_messages_for_call_pgm(app_call_txn))
     assert get_expected_messages_for_call_pgm(app_call_txn) == messages
 
+
+def test_sign_msgpack_call_app_long_approval_app(dongle, app_create_txn):
+    """
+    """
+    app_create_txn.approval_program += b'0'
+    decoded_txn= base64.b64decode(algosdk.encoding.msgpack_encode(app_create_txn))
+    with pytest.raises(speculos.CommException) as excinfo:
+        dongle.exchange(txn_utils.sign_algo_txn(dongle, decoded_txn))
+        
+    assert excinfo.value.sw == 0x6e00
+
+
+def test_sign_msgpack_call_app_long_clear_app(dongle, app_create_txn):
+    """
+    """
+    app_create_txn.clear_program = app_create_txn.clear_program + (32 -len(app_create_txn.clear_program)+1)*b'0'
+    decoded_txn= base64.b64decode(algosdk.encoding.msgpack_encode(app_create_txn))
+    with pytest.raises(speculos.CommException) as excinfo:
+        dongle.exchange(txn_utils.sign_algo_txn(dongle, decoded_txn))
+        
+    assert excinfo.value.sw == 0x6e00
+
+def test_sign_msgpack_call_app_more_than_one_foreign_app(dongle, app_call_txn):
+    """
+    """
+    app_call_txn.foreign_apps.append(81)
+    decoded_txn= base64.b64decode(algosdk.encoding.msgpack_encode(app_call_txn))
+    with pytest.raises(speculos.CommException) as excinfo:
+        dongle.exchange(txn_utils.sign_algo_txn(dongle, decoded_txn))
+        
+    assert excinfo.value.sw == 0x6e00
+
+
+def test_sign_msgpack_call_app_more_than_one_foreign_asset(dongle, app_call_txn):
+    """
+    """
+    app_call_txn.foreign_assets.append(6547014)
+    decoded_txn= base64.b64decode(algosdk.encoding.msgpack_encode(app_call_txn))
+    with pytest.raises(speculos.CommException) as excinfo:
+        dongle.exchange(txn_utils.sign_algo_txn(dongle, decoded_txn))
+        
+    assert excinfo.value.sw == 0x6e00
+
+def test_sign_msgpack_call_app_more_than_two_accounts(dongle, app_call_txn):
+    """
+    """
+    app_call_txn.accounts.append("R4DCCBODM4L7C6CKVOV5NYDPEYS2G5L7KC7LUYPLUCKBCOIZMYJPFUDTKE")
+    decoded_txn= base64.b64decode(algosdk.encoding.msgpack_encode(app_call_txn))
+    with pytest.raises(speculos.CommException) as excinfo:
+        dongle.exchange(txn_utils.sign_algo_txn(dongle, decoded_txn))
+        
+    assert excinfo.value.sw == 0x6e00
 
 
 def test_sign_msgpack_create_app_validate_display(dongle, app_create_txn):
@@ -151,9 +210,7 @@ def test_sign_msgpack_call_app_differnet_on_complete_validate_display(dongle, ap
                         ['clear (sha256)', get_hash_of_pgm(app_call_txn.clear_program).lower()],
                         ['sign', 'transaction']]
         expected_messages = expected_messages[:len(expected_messages)-1] + pgm_messages
-
-        
-        
+       
     logging.info(messages)
     logging.info(expected_messages)
     assert expected_messages == messages
