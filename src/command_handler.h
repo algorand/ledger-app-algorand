aa51053a84decc63a5bcbdd730eecb2b7f407eb1
algoidan <f>
2021-05-26 10:36:40 +0300

    fixing inconsistency between h and c files.

--- src/command_handler.h
+++ src/command_handler.h
@@ -8,9 +8,9 @@
 
 
 
-void parse_input_get_public_key(const uint8_t* buffer, const uint32_t buffer_len, uint32_t* account_id);
+void parse_input_for_get_public_key_command(const uint8_t* buffer, const uint32_t buffer_len, uint32_t* account_id);
 void send_address_to_ui(const uint8_t* public_key, const uint32_t public_key_size);
-char* parse_input_msgpack(const uint8_t * data_buffer, const uint32_t buffer_len, 
+char* parse_input_for_msgpack_command(const uint8_t * data_buffer, const uint32_t buffer_len, 
                         uint8_t* current_tnx_buffer, const uint32_t current_tnx_buffer_size, uint32_t *current_tnx_buffer_offset,
                         txn_t* current_tnx);
 
